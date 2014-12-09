6ff0236a6a8c9bdff80efb485ed5f6feb71e9502
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2014-12-09 19:08:38 +0000

    Create manually os-release symlink

--- base.build/build.sh
+++ base.build/build.sh
@@ -7,3 +7,4 @@ upgrade_packages
 sed -i 's/^#\(en_US\)/\1/' /etc/locale.gen
 locale-gen
 install -m 644 $(temp_content 'LANG=en_US.UTF-8') /etc/locale.conf
+ln -s /usr/lib/os-release /etc/os-release
