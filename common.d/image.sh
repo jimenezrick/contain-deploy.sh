c16379f0fda67a3fb53c4a06f4f9f6b8670c45ed
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2015-01-24 11:53:27 +0000

    Cosmetic

--- common.d/image.sh
+++ common.d/image.sh
@@ -9,7 +9,7 @@ build_dir() {
 create_base() {
 	local image=$(image_dir base)
 
-	echo '* Creating base'
+	echo '==> Creating base'
 	rm -rf $image
 	mkdir -p $image
 	pacstrap -c -d -M $image $base_packages
@@ -20,7 +20,7 @@ copy_image() {
 	local src_image=$(image_dir $1)
 	local dst_image=$(image_dir $2)
 
-	echo "* Copying $1 as $2"
+	echo "==> Copying $1 as $2"
 	rm -rf $dst_image
 	cp -r --preserve=all $src_image $dst_image
 }
@@ -33,7 +33,7 @@ build_image() {
 	if [[ -x $build/build.sh ]]
 	then
 		bootstrap_build $1
-		echo "* Building $1"
+		echo "==> Building $1"
 		systemd-nspawn -D $image /build/build.sh
 		rm -rf $image/build
 	fi
@@ -43,7 +43,7 @@ bootstrap_build() {
 	local image=$(image_dir $1)
 	local build=$(build_dir $1)
 
-	echo "* Bootstrapping $1"
+	echo "==> Bootstrapping $1"
 	cp -r $build $image/build
 	cp -r $basedir/build.d $image/build
 	cp $basedir/build-common.sh $image/build
@@ -53,6 +53,6 @@ compress_image() {
 	local image=$(image_dir $1)
 	local timestamp=$(cat $image/etc/systemd-container-timestamp)
 
-	echo "* Compressing $1"
+	echo "==> Compressing $1"
 	tar -c -f $1-$timestamp.tgz -z --transform s/^$image/$1-$timestamp/ $image
 }
