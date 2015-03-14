7a50702283ad9d66aea00ca2aa819f1185aab5a6
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2015-03-14 18:27:02 +0000

    Riemann image finished

--- build.d/package.sh
+++ build.d/package.sh
@@ -6,8 +6,12 @@ install_packages() {
 	pacman --needed --noconfirm -S $*
 }
 
+install_local_packages() {
+	pacman --needed --noconfirm -U $*
+}
+
 install_aur_packages() {
 	chmod o+w /build
-	install_packages $1
-	sudo -u nobody bash -c "cd /build && bash <(curl aur.sh) --skippgpcheck $2"
+	sudo -u nobody bash -c "cd /build && bash <(curl aur.sh) --skippgpcheck $1"
+	install_local_packages /build/$1/$1-*.pkg.tar.xz
 }
--- riemann.build/build.sh
+++ riemann.build/build.sh
@@ -2,6 +2,13 @@
 
 . $(dirname $0)/build-common.sh
 
-packages='jre8-openjdk'
+packages='jre8-openjdk ruby'
 
-install_aur_packages "$packages" riemann
+install_packages $packages
+
+install_aur_packages riemann
+enable_service riemann
+
+gem install riemann-client riemann-tools riemann-dash --no-user-install --no-document
+install -m 644 /build/riemann-dash.service /etc/systemd/system/riemann-dash.service
+enable_service riemann-dash
